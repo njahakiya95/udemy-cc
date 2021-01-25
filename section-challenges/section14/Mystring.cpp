55b31c0da71a07281ad3af98f854cff029ab002e
Nirmal Patel <patelnirmal1325@gmail.com>
2021-01-25 15:52:22 -0500

    updated Mystring.cpp

--- section-challenges/section14/Mystring.cpp
+++ section-challenges/section14/Mystring.cpp
@@ -27,3 +27,19 @@ Mystring::Mystring(const Mystring &source)
         str = new char[std::strlen(source.str) + 1];
         str::strcpy(str, source.str); 
     }
+    
+//Destructor
+Mystring::~Mystring() {
+    delete [] str; 
+}
+
+//Display method
+void Mystring::display() const {
+    std::cout << str << " : " << get_length() << std::endl;
+}
+
+//length getter
+int Mystring::get_length() const { return std::strlen(str); }
+
+//string getter
+const char *Mystring::get_str() const { return str; }
