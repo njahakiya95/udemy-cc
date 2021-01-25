a8f578f65cf495623f9911df7f66513debaa9584
Nirmal Patel <patelnirmal1325@gmail.com>
2021-01-25 15:48:11 -0500

    updated Mystring.cpp

--- section-challenges/section14/Mystring.cpp
+++ section-challenges/section14/Mystring.cpp
@@ -1,5 +1,29 @@
-class Mystring {
+#include <cstring>
+#include <iostream>
+#include "Mystring.h"
+
+//No-args constructor
+Mystring::Mystring()
+    : str{nullptr} {
+        str = new char[1]; 
+        *str = '\0'; 
+    }
+
+//Overloaded constructor
+Mystring::Mystring(const char *s)
+    : str {nullptr} {
+        if(s == nullptr) {
+            str = new char[1];
+            *str = '\0';
+        } else {
+            str = new char[std::strlen(s) + 1];
+            std::strcpy(str, s);
+        }
+    }
     
-private:
-    char *str; //
-}
+//Copy constructor
+Mystring::Mystring(const Mystring &source)
+    : str{nullptr} {
+        str = new char[std::strlen(source.str) + 1];
+        str::strcpy(str, source.str); 
+    }
